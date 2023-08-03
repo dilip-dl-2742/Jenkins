96f2817af10ec6b52bddec47daee744aaebb00d5
Dilip DL <99382764+dilip-dl-2742@users.noreply.github.com>
2023-08-03 06:27:18 -0400

    Update sharedLib.groovy

--- vars/sharedLib.groovy
+++ vars/sharedLib.groovy
@@ -1,6 +1,6 @@
 def call() {
       sh 'docker pull tomcat:latest'
-      sh 'docker run -d --name Tomcat-shareLib -p 8008:8080 tomcat:latest'
+      sh 'docker run -d --name Tomcat -p 8000:8080 tomcat:latest'
       sh 'docker ps -a'
       echo 'Congratulations! You successfull created a tomcat container'
 }
